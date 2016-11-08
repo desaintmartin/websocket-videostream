1b99f23361e19fc0574a749077cc83eb2296223a
Cédric de Saint Martin <cedric.desaintmartin@francetv.fr>
2016-11-08 17:32:09 +0100

    fixup mjpeg websocket init.

--- client/video-client.js
+++ client/video-client.js
@@ -1,8 +1,5 @@
 var XMLHttpRequestPromise = require('xhr-promise');
 
-var videoDecoder = require('./decoders/video-decoder');
-var canvasDecoder = require('./decoders/canvas-decoder');
-
 var mimeCodec = null;
 var xhrPromise = new XMLHttpRequestPromise();
 
@@ -26,12 +23,12 @@ function initializeVideo() {
   var canvas = document.getElementById('cid');
   if (mimeCodec === 'video/x-msvideo') {
     console.log('Receiving MJPEG, decoding through canvas.');
-    canvasDecoder();
+    require('./decoders/canvas-decoder')();
   } else if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
     console.log('Browser supports ' + mimeCodec + ' video codec.');
     video.style.display = 'block';
     canvas.style.display = 'none';
-    videoDecoder(mimeCodec);
+    require('./decoders/video-decoder')(mimeCodec);
   } else {
     throw new Error(mimeCodec + ' not supported, aborting.');
   }
